66aa7c665cc7d33ff168fa4ecd7fa85bf3582f0d
Michael Vorburger <mike@vorburger.ch>
2015-04-19 17:55:52 +0200

    integrated Web Server

---
+++ TODO.md
@@ -0,0 +1,20 @@
+## ToDo
+
+[ ] Web Server handles POST to update JS files, under fixed folder
+
+[ ] Bundled BlocklyCraft (using POST instead of GET)
+
+[ ] BlocklyCraft: Translate Italian-only blocks
+
+[ ] Maven build instead of build.xml?
+
+
+## Done
+
+# 2015.04.19
+[X] Given hugely successful Devoxx4Kids workshop in Lugano, Ticino, Switzerland using this ScriptCraft + BlocklyCraft ;)
+[X] Integrated Web Server
+      * Can serve static files
+      * HTTP host and port configured via -Dorg.scriptcraftjs.webserver.WebServer.port and *.host
+      * HTTP Request/Response details logging intentionally disabled
+      * Automatically opens web browser
---
+++ src/main/java/common/org/scriptcraftjs/webserver/ScriptCraftWebServer.java
@@ -0,0 +1,36 @@
+package org.scriptcraftjs.webserver;
+
+import fi.iki.elonen.ServerRunner;
+
+
+/**
+ * Web Server for ScriptCraft.
+ * 
+ * General HTTP server code that is not related to ScriptCraft (or BlocklyCraft) should go into the WebServer parent class. 
+ * 
+ * @author Michael Vorburger & Lauro Canonica
+ */
+public class ScriptCraftWebServer extends WebServer {
+
+	private static final String SCRIPTCRAFT_WWW_DIRECTORY = "scriptcraft/www";
+
+	public ScriptCraftWebServer() {
+		super(SCRIPTCRAFT_WWW_DIRECTORY);
+	}
+
+	public ScriptCraftWebServer(String wwwroot) {
+		super(wwwroot);
+	}
+
+	public String getStartedLogMessage() {
+		return "HTTP web server for content in " + SCRIPTCRAFT_WWW_DIRECTORY + " now available on " + getURL();
+	}
+	
+	public static void main(String[] args) {
+		ScriptCraftWebServer webServer = new ScriptCraftWebServer("src/main/js/www");
+		System.out.println(webServer.getStartedLogMessage());
+		webServer.openURL();
+		ServerRunner.executeInstance(webServer.createServer());
+	}
+	
+}
