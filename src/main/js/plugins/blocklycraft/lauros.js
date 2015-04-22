e005698f10f89a9989f161112fbddcd4a458fe0f
Michael Vorburger <mike@vorburger.ch>
2015-04-22 23:03:19 +0200

    some minor reformatting, and fixed a typo

--- src/main/js/plugins/blocklycraft/lauros.js
+++ src/main/js/plugins/blocklycraft/lauros.js
@@ -4,19 +4,19 @@ var scriptFolder = 'scriptcraft/plugins/blocklycraft';
 var fileObject = new java.io.File(scriptFolder);
 
 function watchMyFiles(folder){
-		var names=folder.list();
-		for (var i=0; i<names.length; i++){
-			var fname=scriptFolder+"/"+names[i]; // TODO "\\" instead of "/" needed on Win, or OK? 
-			console.log('watching '+fname );
-			utils.watchFile(fname , function( fileModified ) { // refresh after any file modification
-				refresh();  
+		var names = folder.list();
+		for (var i=0; i<names.length; i++) {
+			var fname = scriptFolder + "/" + names[i]; // TODO "\\" instead of "/" needed on Win, or OK? 
+			console.log('watching '+ fname);
+			utils.watchFile(fname , function( fileModified) { // refresh after any file modification
+				refresh();
 			});
 		}
 }
-function onChangeRefresh(){
-	console.log( 'Monitoring folder '+scriptFolder);
-	utils.watchFile(scriptFolder , function( folder ) { // check the folder
-		console.log( folder + ' has changed !');
+function onChangeRefresh() {
+	console.log( 'Monitoring folder ' + scriptFolder);
+	utils.watchFile(scriptFolder, function( folder ) { // check the folder
+		console.log( folder + ' has changed!');
 		watchMyFiles(fileObject);
 	});
 }
