bc54e726fca8dada81e7d58dfaadda3f7ebd8732
laurocanonica <lauro.canonica@gmail.com>
2015-05-20 11:50:08 +0200

    renamed files, reorganized functions, added info in the README.txt

--- src/main/js/www/blocklycraft/customblocks-javascript-generator.js
+++ src/main/js/www/blocklycraft/customblocks-javascript-generator.js
@@ -1,3 +1,6 @@
+/*******************************************************************************
+ * Authors: - Lauro Canonica, (Devoxx4kids Lugano 2015.04)
+ */
 
 Blockly.JavaScript['box'] = function(block) {
 	var dropdown_material = block.getFieldValue('Material');
@@ -10,31 +13,17 @@ Blockly.JavaScript['objects'] = function(block) {
 	return code;
 };
 
-Blockly.JavaScript['command'] = function(block) {
-	var fname = Blockly.JavaScript.valueToCode(block, 'param',
-			Blockly.JavaScript.ORDER_ATOMIC);
-	var statements_statements = Blockly.JavaScript.statementToCode(block,
-			'statements');
-	var code = "var lauro_extra = require('./lauro_extra');\ncommand( " + fname
-			+ ", function ( parameters, player ) {";
-	code = code + statements_statements;
-	code = code + "});";
-	return code;
-};
 
 Blockly.JavaScript['drone'] = function(block) {
-	var fname = Blockly.JavaScript.valueToCode(block, 'param',
-			Blockly.JavaScript.ORDER_ATOMIC);
-	var statements_statements = Blockly.JavaScript.statementToCode(block,
-			'statements');
-	var code = "var lauro_extra = require('./lauro_extra');\ncommand( "
-			+ fname
+	var fname = Blockly.JavaScript.valueToCode(block, 'param', Blockly.JavaScript.ORDER_ATOMIC);
+	var statements_statements = Blockly.JavaScript.statementToCode(block, 'statements');
+	var code = "var scriptcraftExtensions = require('./scriptcraftExtensions');\ncommand( " + fname
 			+ ", function ( parameters, player ) {\nvar theDrone = new Drone(player);\ntheDrone.up();\ntheDrone.chkpt('start');\n";
 	code = code + "var timeoutStop = new Date().getTime()+500;\n"; // set
-																	// maximum
-																	// run time
-																	// for a
-																	// script
+	// maximum
+	// run time
+	// for a
+	// script
 	code = code + statements_statements;
 	code = code + "});";
 	return code;
@@ -53,57 +42,44 @@ Blockly.JavaScript['drone_tree'] = function(block) {
 };
 
 Blockly.JavaScript['circle'] = function(block) {
-	var value_radius = Blockly.JavaScript.valueToCode(block, 'radius',
-			Blockly.JavaScript.ORDER_ATOMIC);
+	var value_radius = Blockly.JavaScript.valueToCode(block, 'radius', Blockly.JavaScript.ORDER_ATOMIC);
 	var dropdown_material = block.getFieldValue('material');
 	var dropdown_fill = block.getFieldValue('fill');
 	// TODO: Assemble JavaScript into code variable.
-	var code = "theDrone.cylinder" + dropdown_fill + "(" + dropdown_material
-			+ "," + value_radius + ",1);\n";
+	var code = "theDrone.cylinder" + dropdown_fill + "(" + dropdown_material + "," + value_radius + ",1);\n";
 	return code;
 };
 
 Blockly.JavaScript['rectangle'] = function(block) {
-	var value_width = Blockly.JavaScript.valueToCode(block, 'width',
-			Blockly.JavaScript.ORDER_ATOMIC);
-	var value_lenght = Blockly.JavaScript.valueToCode(block, 'lenght',
-			Blockly.JavaScript.ORDER_ATOMIC);
+	var value_width = Blockly.JavaScript.valueToCode(block, 'width', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_lenght = Blockly.JavaScript.valueToCode(block, 'lenght', Blockly.JavaScript.ORDER_ATOMIC);
 	var dropdown_material = block.getFieldValue('material');
 	var dropdown_fill = block.getFieldValue('fill');
-	var code = "theDrone.box" + dropdown_fill + "(" + dropdown_material + ","
-			+ value_width + ",1," + value_lenght + ");\n";
+	var code = "theDrone.box" + dropdown_fill + "(" + dropdown_material + "," + value_width + ",1," + value_lenght + ");\n";
 	return code;
 };
 
 Blockly.JavaScript['mountain'] = function(block) {
-	var value_width = Blockly.JavaScript.valueToCode(block, 'width',
-			Blockly.JavaScript.ORDER_ATOMIC);
-	var value_lenght = Blockly.JavaScript.valueToCode(block, 'lenght',
-			Blockly.JavaScript.ORDER_ATOMIC);
-	var value_height = Blockly.JavaScript.valueToCode(block, 'height',
-			Blockly.JavaScript.ORDER_ATOMIC);
-	var code = "lauro_extra.piramid(theDrone," + value_width + ","
-			+ value_lenght + "," + value_height + ");\n";
+	var value_width = Blockly.JavaScript.valueToCode(block, 'width', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_lenght = Blockly.JavaScript.valueToCode(block, 'lenght', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_height = Blockly.JavaScript.valueToCode(block, 'height', Blockly.JavaScript.ORDER_ATOMIC);
+	var code = "scriptcraftExtensions.piramid(theDrone," + value_width + "," + value_lenght + "," + value_height + ");\n";
 	return code;
 };
 
 Blockly.JavaScript['spawner'] = function(block) {
 	var dropdown_name = block.getFieldValue('NAME');
-	var code = "Packages.net.canarymod.Canary.factory().entityFactory.newEntity(Packages.net.canarymod.api.entity.EntityType[('"
-			+ dropdown_name
+	var code = "Packages.net.canarymod.Canary.factory().entityFactory.newEntity(Packages.net.canarymod.api.entity.EntityType[('" + dropdown_name
 			+ "').toUpperCase()], theDrone.getLocation()).spawn();\n";
 	return code;
 };
 
 Blockly.JavaScript['alternator'] = function(block) {
-	var value_width = Blockly.JavaScript.valueToCode(block, 'width',
-			Blockly.JavaScript.ORDER_ATOMIC);
-	var value_lenght = Blockly.JavaScript.valueToCode(block, 'lenght',
-			Blockly.JavaScript.ORDER_ATOMIC);
+	var value_width = Blockly.JavaScript.valueToCode(block, 'width', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_lenght = Blockly.JavaScript.valueToCode(block, 'lenght', Blockly.JavaScript.ORDER_ATOMIC);
 	var material1 = block.getFieldValue('material1');
 	var material2 = block.getFieldValue('material2');
 
-	var code = "lauro_extra.boxAlternate(theDrone," + value_width + ","
-			+ value_lenght + "," + material1 + "," + material2 + ");\n";
+	var code = "scriptcraftExtensions.boxAlternate(theDrone," + value_width + "," + value_lenght + "," + material1 + "," + material2 + ");\n";
 	return code;
 };
--- src/main/js/www/blocklycraft/customblocks.js
+++ src/main/js/www/blocklycraft/customblocks.js
@@ -1,240 +1,172 @@
-	function getObjNames(list, ids) { // extract the needed objects from the main list
-		var shortList = []
-		for (i = 0; i < ids.length; i++) {
-			shortList[i]=[list[ids[i]], "'"+ids[i]+"'"]
-		}
-		return shortList ; 
+/*******************************************************************************
+ * Authors: - Lauro Canonica, (Devoxx4kids Lugano 2015.04)
+ */
+
+var objects = getObjNames(Blockly.Msg.OBJNAMES, [ 141, 142, 175, 81, 92, 54, 58, 61, 64, 65, 66, 68, 37, 46, 50, 51, 27 ]);
+var blocks = getObjNames(Blockly.Msg.OBJNAMES, [ 0, 1, 2, 3, 4, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 30, 49, 55, 56, 73, 79, 80, 89, 129 ]);
+
+// extract the needed objects from the main list
+function getObjNames(list, ids) {
+	var shortList = []
+	for (i = 0; i < ids.length; i++) {
+		shortList[i] = [ list[ids[i]], "'" + ids[i] + "'" ]
 	}
+	return shortList;
+}
 
 
-	var objects = getObjNames(Blockly.Msg.OBJNAMES , [141, 142, 175, 81, 92, 54, 58, 61, 64, 65, 66, 68, 37, 46, 50, 51, 27]);    
-	var blocks = getObjNames(Blockly.Msg.OBJNAMES , [0, 1, 2, 3, 4, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 30, 49, 55, 56, 73, 79, 80, 89, 129]);    
-
-
+// code generated using Blocklyfactory
 Blockly.Blocks['box'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(160);
-		    this.appendDummyInput()
-		        .appendField(Blockly.Msg.M_MATERIAL);
-		    this.appendDummyInput()
-		        .appendField(new Blockly.FieldDropdown(blocks), "Material");
-		    
-		    
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(160);
+		this.appendDummyInput().appendField(Blockly.Msg.M_MATERIAL);
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown(blocks), "Material");
+
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
 
 Blockly.Blocks['objects'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(160);
-		    this.appendDummyInput()
-		        .appendField(Blockly.Msg.M_OBJECT);
-		    this.appendDummyInput()
-		        .appendField(new Blockly.FieldDropdown(objects), "Material");
-		    
-		    
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(160);
+		this.appendDummyInput().appendField(Blockly.Msg.M_OBJECT);
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown(objects), "Material");
 
-Blockly.Blocks['command'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(120);
-		    this.appendValueInput("param")
-		        .setCheck("String")
-		        .appendField("Command");
-		    this.appendStatementInput("statements")
-		        .setCheck("null");
-		    this.setInputsInline(true);
-		    this.setTooltip('');
-		  }
-		};
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
+
 
 Blockly.Blocks['drone'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(120);
-		    this.appendValueInput("param")
-		        .setCheck("String")
-		        .appendField(Blockly.Msg.M_DRONE);
-		    this.appendStatementInput("statements")
-		        .setCheck("null");
-		    this.setInputsInline(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(120);
+		this.appendValueInput("param").setCheck("String").appendField(Blockly.Msg.M_DRONE);
+		this.appendStatementInput("statements").setCheck("null");
+		this.setInputsInline(true);
+		this.setTooltip('');
+	}
+};
 
 Blockly.Blocks['drone_move'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(120);
-		    this.appendDummyInput()
-	        .appendField(Blockly.Msg.M_MOUVEMENT);
-		    this.appendDummyInput()
-		        .appendField(new Blockly.FieldDropdown([
-		                                                [Blockly.Msg.M_MOUVEMENT_UP, "up()"], 
-		                                                [Blockly.Msg.M_MOUVEMENT_DOWN, "down()"], 
-		                                                [Blockly.Msg.M_MOUVEMENT_FWD, "fwd()"], 
-		                                                [Blockly.Msg.M_MOUVEMENT_BACK, "back()"],
-		                                                [Blockly.Msg.M_MOUVEMENT_RIGHT, "right()"], 
-		                                                [Blockly.Msg.M_MOUVEMENT_LEFT, "left()"],
-		                                                [Blockly.Msg.M_MOUVEMENT_TURN_RIGHT, "turn()"],
-		                                                [Blockly.Msg.M_MOUVEMENT_TURN_LEFT, "turn(2)"],
-		                                                [Blockly.Msg.M_MOUVEMENT_BACKTOSTART, "move('start')"],
-		                                                [Blockly.Msg.M_MOUVEMENT_SAVESTART, "chkpt('start')"]
-		                                               ]), "direction");
-		    
-		    
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(120);
+		this.appendDummyInput().appendField(Blockly.Msg.M_MOUVEMENT);
+		this.appendDummyInput().appendField(
+				new Blockly.FieldDropdown([ [ Blockly.Msg.M_MOUVEMENT_UP, "up()" ], [ Blockly.Msg.M_MOUVEMENT_DOWN, "down()" ],
+						[ Blockly.Msg.M_MOUVEMENT_FWD, "fwd()" ], [ Blockly.Msg.M_MOUVEMENT_BACK, "back()" ], [ Blockly.Msg.M_MOUVEMENT_RIGHT, "right()" ],
+						[ Blockly.Msg.M_MOUVEMENT_LEFT, "left()" ], [ Blockly.Msg.M_MOUVEMENT_TURN_RIGHT, "turn()" ],
+						[ Blockly.Msg.M_MOUVEMENT_TURN_LEFT, "turn(2)" ], [ Blockly.Msg.M_MOUVEMENT_BACKTOSTART, "move('start')" ],
+						[ Blockly.Msg.M_MOUVEMENT_SAVESTART, "chkpt('start')" ] ]), "direction");
+
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
 Blockly.Blocks['drone_tree'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(120);
-		    this.appendDummyInput()
-	        .appendField(Blockly.Msg.M_SPECIALS);
-		    this.appendDummyInput()
-		        .appendField(new Blockly.FieldDropdown([
-		                                                [Blockly.Msg.OAK, "oak()"], 
-		                                                [Blockly.Msg.BED, "bed()"], 
-		                                                [Blockly.Msg.DOOR, "door( blocks.door_wood )"] 
-		                                               ]), "direction");
-		    
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(120);
+		this.appendDummyInput().appendField(Blockly.Msg.M_SPECIALS);
+		this.appendDummyInput().appendField(
+				new Blockly.FieldDropdown([ [ Blockly.Msg.OAK, "oak()" ], [ Blockly.Msg.BED, "bed()" ], [ Blockly.Msg.DOOR, "door( blocks.door_wood )" ] ]),
+				"direction");
+
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
 
 Blockly.Blocks['spawner'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(20);
-		    this.appendDummyInput()
-		        .appendField(Blockly.Msg.M_ANIMALS);
-		    this.appendDummyInput()
-		        .appendField(new Blockly.FieldDropdown([
-		                                                [Blockly.Msg.ANIMALS_BAT, "Bat"], 
-		                                                [Blockly.Msg.ANIMALS_CHICKEN, "Chicken"], 
-		                                                [Blockly.Msg.ANIMALS_COW, "COW"], 
-		                                                [Blockly.Msg.ANIMALS_PIG, "PIG"], 
-		                                                [Blockly.Msg.ANIMALS_RABBIT, "RABBIT"], 
-		                                                [Blockly.Msg.ANIMALS_WOLF, "WOLF"], 
-		                                                [Blockly.Msg.ANIMALS_SHEEP, "Sheep"], 
-		                                                [Blockly.Msg.ANIMALS_HORSE, "Horse"], 
-		                                                [Blockly.Msg.ANIMALS_OCELOT, "OCELOT"]
-		                                                ]), "NAME");
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(20);
+		this.appendDummyInput().appendField(Blockly.Msg.M_ANIMALS);
+		this.appendDummyInput().appendField(
+				new Blockly.FieldDropdown([ [ Blockly.Msg.ANIMALS_BAT, "Bat" ], [ Blockly.Msg.ANIMALS_CHICKEN, "Chicken" ], [ Blockly.Msg.ANIMALS_COW, "COW" ],
+						[ Blockly.Msg.ANIMALS_PIG, "PIG" ], [ Blockly.Msg.ANIMALS_RABBIT, "RABBIT" ], [ Blockly.Msg.ANIMALS_WOLF, "WOLF" ],
+						[ Blockly.Msg.ANIMALS_SHEEP, "Sheep" ], [ Blockly.Msg.ANIMALS_HORSE, "Horse" ], [ Blockly.Msg.ANIMALS_OCELOT, "OCELOT" ] ]), "NAME");
 
-		    
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
 
 Blockly.Blocks['circle'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(260);
-		    this.appendDummyInput()
-		        .appendField(Blockly.Msg.CIRCLE);
-		    this.appendDummyInput()
-		        .appendField(new Blockly.FieldDropdown([["vuoto", "0"], ["pieno", " "]]), "fill");
-		    this.appendValueInput("radius")
-		        .setCheck("Number")
-		        .appendField(Blockly.Msg.RADIUS);
-		    this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown(blocks), "material");
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(260);
+		this.appendDummyInput().appendField(Blockly.Msg.CIRCLE);
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown([ [ Blockly.Msg.EMPTY, "0" ], [ Blockly.Msg.FULL, " " ] ]), "fill");
+		this.appendValueInput("radius").setCheck("Number").appendField(Blockly.Msg.RADIUS);
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown(blocks), "material");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
 
 Blockly.Blocks['rectangle'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(260);
-		    this.appendDummyInput()
-		        .appendField(Blockly.Msg.RECTANGLE);
-		    this.appendDummyInput()
-		        .appendField(new Blockly.FieldDropdown([[Blockly.Msg.EMPTY, "0"], [Blockly.Msg.FULL, " "]]), "fill");
-		    this.appendValueInput("width")
-		        .setCheck("Number")
-		        .appendField(Blockly.Msg.WIDTH);
-		    this.appendValueInput("lenght")
-		        .setCheck("Number")
-		        .appendField(Blockly.Msg.LENGTH);
-		    this.appendDummyInput()
-	        .appendField(Blockly.Msg.M_MATERIAL);
-		    this.appendDummyInput()
-	        .appendField(new Blockly.FieldDropdown(blocks), "material");
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(260);
+		this.appendDummyInput().appendField(Blockly.Msg.RECTANGLE);
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown([ [ Blockly.Msg.EMPTY, "0" ], [ Blockly.Msg.FULL, " " ] ]), "fill");
+		this.appendValueInput("width").setCheck("Number").appendField(Blockly.Msg.WIDTH);
+		this.appendValueInput("lenght").setCheck("Number").appendField(Blockly.Msg.LENGTH);
+		this.appendDummyInput().appendField(Blockly.Msg.M_MATERIAL);
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown(blocks), "material");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
 
 Blockly.Blocks['mountain'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(260);
-		    this.appendDummyInput()
-		        .appendField("mountain");
-		    this.appendValueInput("width")
-		        .setCheck("Number")
-		        .appendField("width");
-		    this.appendValueInput("lenght")
-	        .setCheck("Number")
-	        .appendField("lenght");
-		    this.appendValueInput("height")
-	        .setCheck("Number")
-	        .appendField("height");
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(260);
+		this.appendDummyInput().appendField("mountain");
+		this.appendValueInput("width").setCheck("Number").appendField("width");
+		this.appendValueInput("lenght").setCheck("Number").appendField("lenght");
+		this.appendValueInput("height").setCheck("Number").appendField("height");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
 
 Blockly.Blocks['alternator'] = {
-		  init: function() {
-		    this.setHelpUrl('http://www.example.com/');
-		    this.setColour(260);
-		    this.appendDummyInput()
-		        .appendField(Blockly.Msg.RECTANGLE);
-		    this.appendValueInput("width")
-		        .setCheck("Number")
-		        .appendField(Blockly.Msg.WIDTH);
-		    this.appendValueInput("lenght")
-	        .setCheck("Number")
-	        .appendField(Blockly.Msg.LENGTH);
-		    this.appendDummyInput()
-	        .appendField(Blockly.Msg.M_MATERIAL);
-		    this.appendDummyInput()
-	        .appendField(new Blockly.FieldDropdown(blocks), "material1");
-		    this.appendDummyInput()
-	        .appendField(new Blockly.FieldDropdown(blocks), "material2");
-		    this.setInputsInline(true);
-		    this.setPreviousStatement(true);
-		    this.setNextStatement(true);
-		    this.setTooltip('');
-		  }
-		};
+	init : function() {
+		this.setHelpUrl('');
+		this.setColour(260);
+		this.appendDummyInput().appendField(Blockly.Msg.RECTANGLE);
+		this.appendValueInput("width").setCheck("Number").appendField(Blockly.Msg.WIDTH);
+		this.appendValueInput("lenght").setCheck("Number").appendField(Blockly.Msg.LENGTH);
+		this.appendDummyInput().appendField(Blockly.Msg.M_MATERIAL);
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown(blocks), "material1");
+		this.appendDummyInput().appendField(new Blockly.FieldDropdown(blocks), "material2");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('');
+	}
+};
